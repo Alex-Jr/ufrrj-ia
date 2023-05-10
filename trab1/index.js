469cbb8135834fbc792b77255733caa93cb116fd
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2023-05-10 19:43:20 -0300

    comentários e algumas melhorias

--- trab1/index.js
+++ trab1/index.js
@@ -8,7 +8,7 @@ let matrixDivs = [];
 
 const delayMovimento = 50; // 1 segundo
 
-const tamanhoGrid = 10;
+const tamanhoGrid = 15;
 
 let xRobo = 0;
 let yRobo = 0;
@@ -33,7 +33,6 @@ const DIRECOESTXT = [
   "OESTE",
   "NOROESTE"
 ];
-
 
 const DIREITA = 1;
 const ESQUERDA = -1;
@@ -65,6 +64,7 @@ function começar() {
 function parar() {
   if(timer) {
     clearInterval(timer)
+    timer = undefined;
   }
 }
 
@@ -75,8 +75,9 @@ function checaObstaculo(x, y) {
     || matrixBlocos[x][y] === 2
 }
 
+// mover sobreescreve a meta, então eu tenho que checar assim
 function checaMeta(x, y) {
-  return matrixBlocos[x][y] === 3
+  return x === xMeta && y === yMeta
 }
 
 function calculaMovimento(direcao) {
@@ -85,32 +86,32 @@ function calculaMovimento(direcao) {
 
   switch (direcao) {
     case NORTE:
-      xNovo = xRobo;
-      yNovo = yRobo - 1;
+      xNovo = xRobo - 1;
+      yNovo = yRobo;
       break;
     case NORDESTE:
-      xNovo = xRobo + 1;
-      yNovo = yRobo - 1;
+      xNovo = xRobo - 1;
+      yNovo = yRobo + 1;
       break;
     case LESTE:
-      xNovo = xRobo + 1;
-      yNovo = yRobo;
+      xNovo = xRobo;
+      yNovo = yRobo + 1;
       break;
     case SUDESTE:
       xNovo = xRobo + 1;
       yNovo = yRobo + 1;
       break;
     case SUL:
-      xNovo = xRobo;
-      yNovo = yRobo + 1;
+      xNovo = xRobo + 1;
+      yNovo = yRobo;
       break;
     case SUDOESTE:
-      xNovo = xRobo - 1;
-      yNovo = yRobo + 1;
+      xNovo = xRobo + 1;
+      yNovo = yRobo - 1;
       break;
     case OESTE:
-      xNovo = xRobo - 1;
-      yNovo = yRobo;
+      xNovo = xRobo;
+      yNovo = yRobo - 1;
       break;
     case NOROESTE:
       xNovo = xRobo - 1;
@@ -123,12 +124,17 @@ function calculaMovimento(direcao) {
   }
 }
 
+// retorna a melhor maneira de girar para uma posição
 function calcularMenorNumeroGiros(direcaoNova) {
-  const totalRotacoes = 8; // Total de possibilidades de rotação
+  // Total de possibilidades de rotação
+  const totalRotacoes = 8; 
+  // calcula a distancia angular para onde queremos chegar
   const distanciaAngular = (direcaoNova - direcaoAtual + totalRotacoes) % totalRotacoes;
+  // verifica qual é a melhor maneira, pois podemos girar de ambos os lados
   const menorNumeroGiros = Math.min(distanciaAngular, totalRotacoes - distanciaAngular);
 
   let direcaoGiro;
+  // se for maior que girar metade, usa o outro lado
   if (distanciaAngular <= totalRotacoes / 2) {
     direcaoGiro = DIREITA;
   } else {
@@ -169,18 +175,30 @@ function buscaProfundidade(manual = false) {
   const cust = [];
   const mov = []
 
+  // calcula o custo de cada movimento
   for(const direcao of DIRECOES) {
+    // verifica como chegamos lá
     const { xNovo, yNovo } = calculaMovimento(direcao)
+
+    // verifica se tem um obstaculo, ou seja não podemos ir para lá
     if(checaObstaculo(xNovo, yNovo)) {
       cust.push(1000);
       mov.push({direcaoGiro: undefined, qntGiro: undefined})
       continue
     }
 
+    // acha a melhor maneira de girar para essa direção específica
     const { direcaoGiro, menorNumeroGiros } = calcularMenorNumeroGiros(direcao);
 
+    // adiciona o custo do bloco atual
+    // quando é menor que 0 quer dizer que ja estivemos nesse bloco
+    // cada vez que passamos lá denovo adiciona mais um de custo
     const custoBloco = matrixBlocos[xNovo][yNovo] > 0 ? 0 : -matrixBlocos[xNovo][yNovo];
+
+    // custo de girar + custo de andar + penalidade de ja ter passado lá
     cust.push(menorNumeroGiros + 1 + custoBloco);
+
+    // salva a maneira para girar, pois aidna não decidimos como vamos se movimentar
     mov.push({ direcaoGiro, qntGiro: menorNumeroGiros } )
   }
 
@@ -188,7 +206,9 @@ function buscaProfundidade(manual = false) {
     imprimeCusto(cust);
   }
   
+  // acha a posição que queremos escolher
   const index = cust.indexOf(encontrarNumero(cust));
+  // acha como temos que girar
   const giro = mov[index];
 
   if(manual) {
@@ -196,20 +216,21 @@ function buscaProfundidade(manual = false) {
     console.log(giro);
   }
 
-
-
+  // gira o necessário
   for(let i = 0; i < giro.qntGiro; i++) {
     girarRobo(giro.direcaoGiro);
   }
+
+  // se move para frente
   moverRobo();
 }
 
+// isso irá rodar a cada execução
 function loop() {
   buscaProfundidade();
 
   if(checaMeta(xRobo, yRobo)) {
     parar()
-    alert('acabou')
   }
 }
 
@@ -218,6 +239,7 @@ function loop() {
 function girarRobo(movimento) {
   qntGiro += 1;
 
+  // isso garante que o robo se mova de  7 -> 0 ou de 0 -> 7
   switch (movimento) {
     case DIREITA: 
       direcaoAtual = (direcaoAtual + 1) % 8;
@@ -227,15 +249,22 @@ function girarRobo(movimento) {
       break;
   }
 
+  // atualiza visualmente como o robo está virado
   const roboElement = document.querySelector(".robo");
   roboElement.style.transform = `rotate(${[(direcaoAtual) * 45]}deg)`;
 }
 
-let oldState = 0;
+let antigoValor = 0;
 // robo se move somente para frente
-function moverRobo() { 
+function moverRobo(manual = false) { 
+  // calcula para onde vamos se mover
   const { xNovo, yNovo } = calculaMovimento(direcaoAtual)
 
+  if(manual) {
+    console.log('x', xNovo, 'y', yNovo); 
+  }
+
+  // calcula se pode se mover para lá
   if(checaObstaculo(xNovo, yNovo)) return;
 
   qntMovimento += 1;
@@ -243,77 +272,50 @@ function moverRobo() {
   // salva a div do robo
   const robo = matrixDivs[xRobo][yRobo].childNodes[0];
 
-  // vai adicionando custo no bloco que ele ja estava
-  matrixBlocos[xRobo][yRobo] = oldState - 1;
+  // blocos que o robo já passou tem seu custo aumentado
+  matrixBlocos[xRobo][yRobo] = antigoValor - 1;
+  matrixDivs[xRobo][yRobo].innerText = String(antigoValor - 1).replace('-', '');
   matrixDivs[xRobo][yRobo].className = 'bloco';
-  matrixDivs[xRobo][yRobo].removeChild(robo)
 
   // move o robo
   xRobo = xNovo;
   yRobo = yNovo;
 
-  // atualiza a div onde o robo está
-  oldState = matrixBlocos[xRobo][yRobo];
+  // como a posição atual do robo sobreescreve o valor antigo do bloco
+  // salvamos ele na variavel antigoValor
+  antigoValor = matrixBlocos[xRobo][yRobo];
 
+  // mostra visualmente e logicamente onde o robo está
   matrixBlocos[xRobo][yRobo] = 1;
+  matrixDivs[xRobo][yRobo].innerText = '';
   matrixDivs[xRobo][yRobo].classList.add('bloco-robo');
   matrixDivs[xRobo][yRobo].appendChild(robo)
-}
 
-function configurarJogo() {
-  main();
-
-  custoAtual = 0;
-  xRobo = 0
-  yRobo = 0
-
-
-  // configura o robo inciial
-  matrixBlocos[xRobo][yRobo] = 1
-  matrixDivs[xRobo][yRobo].className = 'bloco'
-  matrixDivs[xRobo][yRobo].classList.add('bloco-robo')
-  const robo = document.createElement('div');
-  robo.classList.add('robo');
-  matrixDivs[xRobo][yRobo].appendChild(robo);
-
-  
-  for (let i = 0; i < tamanhoGrid; i++) {
-    for (let j = 0; j < tamanhoGrid; j++) {
-      if(i === xRobo && j === yRobo) continue;
-      if(i === xMeta && j === yMeta) continue;
-
-      matrixBlocos[i][j] = 0
-      matrixDivs[i][j].className = 'bloco'
-
-      if (Math.random() < 0.2 ) {
-        matrixBlocos[i][j] = 2;
-        matrixDivs[i][j].classList.add('bloco-obstaculo');
-      }
-    }
+  if(manual) {
+    console.log(checaMeta(xRobo, yRobo));
   }
-
-  matrixBlocos[xMeta][yMeta] = 3;
-  matrixDivs[xMeta][yMeta].classList.add('bloco-meta')
 }
 
-function main() {
+function configurar() {
+  const tamanho = tamanhoGrid;
+  parar()
+
+  // GRID 
   const mainDiv = document.querySelector('#main');
-  // deixa em branco a main div
+  // apaga antigo setup
   mainDiv.innerHTML = ''
-
-  // configura o estilo da grid
-  mainDiv.style.gridTemplateColumns = `repeat(${tamanhoGrid}, 1fr)` 
-  mainDiv.style.gridTemplateRows = `repeat(${tamanhoGrid}, 1fr)` 
-
-  // apaga antigo jogo
   matrixBlocos = [];
   matrixDivs = [];
 
+  // configura o estilo da grid
+  mainDiv.style.gridTemplateColumns = `repeat(${tamanho}, 1fr)` 
+  mainDiv.style.gridTemplateRows = `repeat(${tamanho}, 1fr)` 
+
   // cria a grid e salva os status nos arrays blocoStatus e divs
-  for (let i = 0; i < tamanhoGrid; i++) {
+  for (let i = 0; i < tamanho; i++) {
     matrixBlocos[i] = [];
     matrixDivs[i] = []
-    for (let j = 0; j < tamanhoGrid; j++) {
+    for (let j = 0; j < tamanho; j++) {
       const bloco = document.createElement('div');
 
       bloco.classList.add('bloco');
@@ -324,14 +326,56 @@ function main() {
       matrixDivs[i][j] = bloco;
     }
   }
+
+  // ROBO, METAS E OBSTACULOS
+  custoAtual = 0;
+  xRobo = 0
+  yRobo = 0
+
+  // cria a div do robo
+  matrixBlocos[xRobo][yRobo] = 1
+  matrixDivs[xRobo][yRobo].className = 'bloco'
+  matrixDivs[xRobo][yRobo].classList.add('bloco-robo')
+
+  // cria o robo em si
+  const robo = document.createElement('div');
+  robo.classList.add('robo');
+  matrixDivs[xRobo][yRobo].appendChild(robo);
+
+  
+  // cria a meta
+  matrixBlocos[xMeta][yMeta] = 3;
+  matrixDivs[xMeta][yMeta].classList.add('bloco-meta')
+
+  
+  // Configurar obstaculos
+  for (let i = 0; i < tamanhoGrid; i++) {
+    for (let j = 0; j < tamanhoGrid; j++) {
+      // não pode ser no robo
+      if(i === xRobo && j === yRobo) continue;
+      // não pode ser na meta
+      if(i === xMeta && j === yMeta) continue;
+
+      matrixBlocos[i][j] = 0
+      matrixDivs[i][j].className = 'bloco'
+
+      // 0.2% de chance de haver um obstaculo
+      // isso pode gerar caminhos impossíveis por aleatoriedade
+      if (Math.random() < 0.2 ) {
+        matrixBlocos[i][j] = 2;
+        matrixDivs[i][j].classList.add('bloco-obstaculo');
+      }
+    }
+  }
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
-  main()
+  configurar()
 })
 
 document.addEventListener('keydown', function(event) {
-  if (event.code === 'KeyW') moverRobo()
+  if (event.code === 'KeyW') moverRobo(true)
 
   if (event.code === 'KeyQ') girarRobo(ESQUERDA)
   
